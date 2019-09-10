6555d6b4ddb928364ba9ad3b2a84f64341e784bb
Diego Moussallem <diegomoussallem@gmail.com>
2019-09-10 14:09:56 +0200

    updating the submodule

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 git submodule add https://github.com/OpenNMT/OpenNMT-py
+git submodule init
+git submodule update
+git clone --recurse-submodules https://github.com/OpenNMT/OpenNMT-py
 pip3 install -r requirements.txt
 python3 -m spacy download en
 python3 -m spacy download de
