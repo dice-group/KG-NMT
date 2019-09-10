6a7d7ca79171da62f51f11c1c3ab7cbc4c08fa06
Diego Moussallem <diegomoussallem@gmail.com>
2019-09-10 14:36:17 +0200

    fixing SemKGE file

--- SemKGE_creation.sh
+++ SemKGE_creation.sh
@@ -30,7 +30,7 @@ if [ -f "labels_"$1".ttl.bz2" ] ; then
 fi
 
 
-if [ ! -d "fastText-0.9.1" ]; then
+if [ ! -f "fastText-0.9.1.zip" ]; then
 	wget https://github.com/facebookresearch/fastText/archive/v0.9.1.zip ;
 	unzip v0.9.1.zip ;
 fi
