16df96da5d7fb19ab8db20457fbdaec66d9b82cd
Diego Moussallem <diegomoussallem@gmail.com>
2019-09-16 09:19:57 +0200

    fixing mt metrics path

--- kg-nmt_SemKGE_workflow.sh
+++ kg-nmt_SemKGE_workflow.sh
@@ -20,12 +20,12 @@ python3 $OPEN_NMT_PATH/preprocess.py \
 
 # KGE 
 
-#if [ ! -f "fastText-0.9.1.zip" ]; then
-#./SemKGE_creation.sh en
-#if
-#if [ ! -f "fastText-0.9.1.zip" ]; then
-#./SemKGE_creation.sh de
-#fi
+if [ ! -d "fastText-0.9.1" ]; then
+./SemKGE_creation.sh en
+if
+if [ ! -d "fastText-0.9.1" ]; then
+./SemKGE_creation.sh de
+fi
 
 python3 $OPEN_NMT_PATH/embeddings_to_torch.py -emb_file_enc KGE/$SRC_LAN/all_"$SRC_LAN"_model.vec -emb_file_dec KGE/$TGT_LAN/all_"$TGT_LAN"_model.vec -type word2vec -dict_file $TRAIN_PATH/preprocessed/training-data-"$SRC_LAN-$TGT_LAN".vocab.pt -output_file $TRAIN_PATH/preprocessed/all_kge_"$SRC_LAN-$TGT_LAN"_emb
 
@@ -33,20 +33,20 @@ python3 $OPEN_NMT_PATH/embeddings_to_torch.py -emb_file_enc KGE/$SRC_LAN/all_"$S
 # Training
 echo "Training"
 
-python3  $OPEN_NMT_PATH/train.py -data $TRAIN_PATH/preprocessed/training-data-"$SRC_LAN-$TGT_LAN" -save_model $TRAIN_PATH/models/"$SRC_LAN-$TGT_LAN"+graph -word_vec_size 500 -pre_word_vecs_enc $TRAIN_PATH/preprocessed/all_kge_"$SRC_LAN-$TGT_LAN"_emb.enc.pt -pre_word_vecs_dec $TRAIN_PATH/preprocessed/all_kge_"$SRC_LAN-$TGT_LAN"_emb.dec.pt -feat_merge sum -encoder_type brnn -decoder_type rnn -gpu_rank 0
+python3  $OPEN_NMT_PATH/train.py -data $TRAIN_PATH/preprocessed/training-data-"$SRC_LAN-$TGT_LAN" -save_model $TRAIN_PATH/models/"$SRC_LAN-$TGT_LAN"+graph -word_vec_size 500 -pre_word_vecs_enc $TRAIN_PATH/preprocessed/all_kge_"$SRC_LAN-$TGT_LAN"_emb.enc.pt -pre_word_vecs_dec $TRAIN_PATH/preprocessed/all_kge_"$SRC_LAN-$TGT_LAN"_emb.dec.pt -feat_merge sum -encoder_type brnn -decoder_type rnn -gpu_rank 0 -train_steps 410000 
 
 # Testing
 echo "Testing"
-python3 $OPEN_NMT_PATH/translate.py  -gpu 0 -model $TRAIN_PATH/models/$SRC_LAN-$TGT_LAN+graph_*_e13.pt -src $TRAIN_PATH/training_data/test/source.$SRC_LAN  -tgt $TRAIN_PATH/training_data/test/target.$TGT_LAN -replace_unk -output $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output
+python3 $OPEN_NMT_PATH/translate.py  -gpu 0 -model $TRAIN_PATH/models/$SRC_LAN-$TGT_LAN+graph_step_410000.pt -src $TRAIN_PATH/training_data/test/source.$SRC_LAN  -tgt $TRAIN_PATH/training_data/test/target.$TGT_LAN -replace_unk -output $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output
 
 # MT automatic
-#echo "BLEU + METEOR + CHRF"
+echo "BLEU + METEOR + CHRF"
 
-BLEU - most used metric for MT, word overlap	
-perl mt_metrics/scripts/multi-bleu.perl $TRAIN_PATH/training_data/test/target.$TGT_LAN < $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output	
+#BLEU - most used metric for MT, word overlap	
+perl mt_metrics/multi-bleu.perl  $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output < $TRAIN_PATH/training_data/test/target.$TGT_LAN
 	
 #METEOR - kind of stem overlap	
-java -Xmx2G -jar/mt_metrics/meteor-1.5/meteor-1.5.jar $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output $TRAIN_PATH/training_data/test/target.$TGT_LAN -norm -l de	
+java -Xmx2G -jar mt_metrics/meteor-1.5/meteor-1.5.jar $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output $TRAIN_PATH/training_data/test/target.$TGT_LAN -norm -l de	
 	
 #chrF - character based metric	
-python3 /mt_metrics/chrF/chrF.py -b 3 -H $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output -R $TRAIN_PATH/training_data/test/target.$TGT_LAN
+python3 mt_metrics/chrF/chrF.py -b 3 -H $TRAIN_PATH/training_data/test/hypo.$SRC_LAN-$TGT_LAN.test.output -R $TRAIN_PATH/training_data/test/target.$TGT_LAN
