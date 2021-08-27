24fb2b90232c6e94663cba881f7a18e9441b2b1e
Nikit Srivastava <nikit91@gmail.com>
2021-08-27 11:44:55 +0200

    changing submodule remote

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 git submodule add https://github.com/nikit91/OpenNMT-py
 git submodule init
 git submodule update
-git clone --recurse-submodules https://github.com/nikit91/OpenNMT-py
 pip3 install -r requirements.txt
 python3 -m spacy download en
 python3 -m spacy download de
