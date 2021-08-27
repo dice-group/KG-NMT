21bd840cac4f2f1fa47d970a117b8fe7c83a6dbc
Nikit Srivastava <nikit91@gmail.com>
2021-08-27 12:09:02 +0200

    removing git modules

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git submodule add https://github.com/nikit91/OpenNMT-py
+git submodule init
 git submodule update --remote
 pip3 install -r requirements.txt
 python3 -m spacy download en
